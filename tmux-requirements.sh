781c5d858f34ee156062689839b043dddd1dc43a
Nod Swal <36979380+nodswal@users.noreply.github.com>
2021-01-18 20:19:42 -0600

    Create tmux-requirements.sh

---
+++ tmux-requirements.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "You must be root to do this." 1>&2
+	sudo $0 $@
+   exit 100
+fi
+
+# CentOS / RedHat Requirements
+# yum install openssl-devel
+# build-essentials probably
+
+echo test
+# Raspbian Requirements
+
+
+
+
+# Ubuntu 20.04 Requirements
+#	apt-get install build-essential -y
+#	apt-get install libssl-dev -y
+
+
+
+# Arch Requirements
+# openssl
